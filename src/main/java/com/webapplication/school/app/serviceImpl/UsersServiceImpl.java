348935523af5d21fdbf26376d0b53339337bfcd9
Vamsi <vamsi9798@gmail.com>
2020-08-18 10:37:45 +0530

    code fix

--- src/main/java/com/webapplication/school/app/serviceImpl/UsersServiceImpl.java
+++ src/main/java/com/webapplication/school/app/serviceImpl/UsersServiceImpl.java
@@ -19,32 +19,32 @@ import com.webapplication.school.app.repository.UserRepository;
 import com.webapplication.school.app.service.UsersService;
 
 @Service
-public class UsersServiceImpl implements UsersService{
+public class UsersServiceImpl implements UsersService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
-	
+
 	@Autowired
 	private JwtUtil jwtUtil;
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(UsersServiceImpl.class);
 
 	/*
-	* Author : vamsinuthalapati (github)
-	*/
+	 * Author : vamsinuthalapati (github)
+	 */
 	@Override
 	public ResponseObject login(LoginRequest userLogin) throws Exception {
-		
+
 		Users users = new Users();
 		users = userRepository.login(userLogin.getUserId(), userLogin.getPassword());
-		
-		if(users != null) {
+
+		if (users != null) {
 			try {
 				authenticationManager.authenticate(
 						new UsernamePasswordAuthenticationToken(userLogin.getUserId(), userLogin.getPassword()));
@@ -56,29 +56,26 @@ public class UsersServiceImpl implements UsersService{
 
 			final String jwt = jwtUtil.generateToken(userDetails);
 
-
 			LOGGER.info("Student login successful");
 			return new ResponseObject(jwt, "Login successful", HttpStatus.OK);
 		}
 		return new ResponseObject(null, "Something went wrong! please try again", HttpStatus.BAD_REQUEST);
 	}
-	
+
 //	@PreAuthorize("hasAnyRole('ADMIN', 'REGULAR')")
 	@Override
 	public ResponseObject updatePassword(UpdatePassword updatePassword) {
-		
+
 		Users users = userRepository.findUserById(updatePassword.getUserId());
-		
-		if(users != null) {
+
+		if (users != null) {
 			if (updatePassword.getNewPassword().equals(updatePassword.getConfirmNewPassword())) {
 				userRepository.updatePwd(updatePassword.getNewPassword(), updatePassword.getUserId());
-				
+
 				return new ResponseObject("password updated successfully", null, HttpStatus.OK);
 			}
 		}
 		return new ResponseObject(null, "Something went wrong! please check your credentials", HttpStatus.BAD_REQUEST);
 	}
 
-public void newBranch(){
-
-
+}
